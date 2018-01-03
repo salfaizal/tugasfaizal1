a0ef6eb16b94d484f68844e44b166d223bf6ca0a
faizal ardiansyah <32302330+salfaizal@users.noreply.github.com>
2018-01-03 12:16:08 +0700

    mengambil nilai max tiap kriteria

--- tugas1.cpp
+++ tugas1.cpp
@@ -13,3 +13,9 @@ int main(){
 		}cout<<endl;
 	}// Akhir dari Matrix X
 
+	// Mengambil nilai max tiap krateria
+	cout <<endl<< "MATRIKS R : "<<endl;	
+	double mc[5];
+	for(int i=0;i<5;i++){
+		mc[i+1]=max(ac[0][i],max(ac[1][i],ac[2][i]));
+	}
